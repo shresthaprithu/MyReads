56599f5bd1c06667af1f8584e2b31b7a0f9b4dea
Prithu Shrestha <shresthaprithu@gmail.com>
2019-09-17 09:09:18 +1000

    feat: passed props to search results
    making search results show all listed books without filter at first.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import SearchBooksInput from './SearchBooksInput'
 import SearchResults from './SearchResults'
 
-function SearchBooks(props) {
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <Link to="/">
-          <button className={'close-search'}>Add a Book</button>
-        </Link>
-        <SearchBooksInput/>
+class SearchBooks extends Component{
+  render() {
+    const { books } = this.props;
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/">
+            <button className={'close-search'}>Add a Book</button>
+          </Link>
+          <SearchBooksInput/>
+        </div>
+        <SearchResults books={books}/>
       </div>
-      <SearchResults/>
-    </div>
-  )
+    )
+  }
 }
 
 export default SearchBooks
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Book from "./Book";
 
-function SearchBooksResults(props) {
+const SearchResults = props => {
+  const { books } = props;
   return (
     <div className="search-books-results">
-      <ol className="books-grid"></ol>
+      <ol className="books-grid">
+        {books.map(book => (
+          <Book key={book.id} book={book} shelf="none" />
+        ))}
+      </ol>
     </div>
   )
 }
 
-export default SearchBooksResults
+export default SearchResults
