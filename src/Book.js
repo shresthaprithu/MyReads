a8b495835a2100374a2ed2fa3fc41ebb838afa45
Prithu Shrestha <shresthaprithu@gmail.com>
2019-09-22 15:49:30 +1000

    feat: missing book cover and authors now display placeholder and text

--- src/Book.js
+++ src/Book.js
@@ -3,25 +3,19 @@ import BookShelfChanger from "./BookShelfChanger";
 
 const Book = props => {
   const { book, shelf, changeShelf } = props;
-  // console.log('book.imageLinks.thumbnail', book.imageLinks)
   return (
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover"
-          style =
-            {
-              {
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})`,
-              }
-            }
-          />
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${
+                book.imageLinks
+                    ? book.imageLinks.thumbnail
+                    : 'https://via.placeholder.com/128x193'
+            })`}}/>
           <BookShelfChanger book={book} shelf={shelf} changeShelf={changeShelf} />
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors && book.authors.join(' and ')}</div>
+        <div className="book-authors">{book.authors ? book.authors.join(', ') : 'Author is unknown'}</div>
       </div>
     </li>
   )
