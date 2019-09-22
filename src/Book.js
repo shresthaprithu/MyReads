cd43345e6d62327cd2cba969e1d4b67d6a706f66
Prithu Shrestha <shresthaprithu@gmail.com>
2019-09-22 15:05:58 +1000

    feat: search works correctly when a book does not have a thumbnail or an author

--- src/Book.js
+++ src/Book.js
@@ -3,15 +3,25 @@ import BookShelfChanger from "./BookShelfChanger";
 
 const Book = props => {
   const { book, shelf, changeShelf } = props;
+  // console.log('book.imageLinks.thumbnail', book.imageLinks)
   return (
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+          <div className="book-cover"
+          style =
+            {
+              {
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})`,
+              }
+            }
+          />
           <BookShelfChanger book={book} shelf={shelf} changeShelf={changeShelf} />
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors.join(', ')}</div>
+        <div className="book-authors">{book.authors && book.authors.join(' and ')}</div>
       </div>
     </li>
   )
